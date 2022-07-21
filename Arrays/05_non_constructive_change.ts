887ca81093514f8304dd65329b2f7da4c1698664
David Alejandro Garcia Lopez <d.alex10092@gmail.com>
2022-07-20 22:29:32 -0500

    Clean code

--- Arrays/05_non_constructive_change.ts
+++ Arrays/05_non_constructive_change.ts
@@ -1,11 +1,7 @@
 export function nonConstructibleChange(coins: number[]) {
     if(!coins.length)
       return 1;
-    
-    const sorterCoins = coins.sort(function(a,b) { 
-      return a - b;
-    });
-    
+      
     let minValue = 0;
     
     for(let coin of coins) {
